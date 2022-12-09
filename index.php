2f8594acd19fb6a8746f973a4600e2c97f51c0f5
nedh <nedh.design@gmail.com>
2022-12-09 11:04:53 +0100

    rectification screenshot.jpg

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
         <div class="row center opac">
             <img class="livredor center" src="img/livredor.png" alt="accueil">
         </div>
-        <div class="row center wrap">
+        <div class="row btns center wrap">
             <a href="inscription.php"><button class="animbottom">Inscription</button></a>          
             <a href="connexion.php"><button class="animbottom">Connexion</button></a>
         </div>
